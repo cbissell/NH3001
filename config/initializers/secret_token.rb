72d8e40451843cbeffb87f7ebbcbfb6208e77bad
Chris Bissell <christopher.bissell@oracle.com>
2013-03-29 11:09:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NH3001::Application.config.secret_token = 'f701d80a891d038da31775093be8989dd2d924ad8f3876de5aa38e62f6218ca8867958ff9dabd0e3e18a3ba344111263b5ba8ba260b083d5e13eff78c230e4ff'
